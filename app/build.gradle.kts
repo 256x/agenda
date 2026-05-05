194c9e1e1af85ae0c668b93586893418b2987b76
256x <admin@fumi.day>
2026-05-05 13:25:58 +0900

    chore: bump version to 1.3.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,8 +40,8 @@ android {
         applicationId = "fumi.day.literalagenda"
         minSdk = 26
         targetSdk = 35
-        versionCode = 5
-        versionName = "1.3.0"
+        versionCode = 6
+        versionName = "1.3.1"
     }
 
     buildTypes {
